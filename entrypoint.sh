b08158bd84fcc9ffb4040a3139396541ff0f6add
Sameer Naik <sameer@damagehead.com>
2015-09-06 20:12:17 +0530

    entrypoint.sh: use `[[` in conditional statements

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ DB_NAME=${DB_NAME:-}
 
 # is a mysql container linked?
 # requires that the mysql container has exposed port 3306
-if [ -n "${MYSQL_PORT_3306_TCP_ADDR}" ]; then
+if [[ -n ${MYSQL_PORT_3306_TCP_ADDR} ]]; then
   DB_TYPE=${DB_TYPE:-mysql}
   DB_HOST=${DB_HOST:-${MYSQL_PORT_3306_TCP_ADDR}}
   DB_PORT=${DB_PORT:-${MYSQL_PORT_3306_TCP_PORT}}
@@ -28,7 +28,7 @@ if [ -n "${MYSQL_PORT_3306_TCP_ADDR}" ]; then
   DB_NAME=${DB_NAME:-${MYSQL_ENV_MYSQL_DATABASE}}
 fi
 
-if [ -z "${DB_HOST}" ]; then
+if [[ -z ${DB_HOST} ]]; then
   echo "ERROR: "
   echo "  Please configure the database connection."
   echo "  Cannot continue without a database. Aborting..."
@@ -38,7 +38,7 @@ fi
 DB_PORT=${DB_PORT:-3306}
 
 # install nginx configuration, if not exists
-if [ -d /etc/nginx/sites-enabled -a ! -f /etc/nginx/sites-enabled/${INVOICE_PLANE_FQDN}.conf ]; then
+if [[ -d /etc/nginx/sites-enabled && ! -f /etc/nginx/sites-enabled/${INVOICE_PLANE_FQDN}.conf ]]; then
   cp /var/cache/invoiceplane/conf/nginx/InvoicePlane.conf /etc/nginx/sites-enabled/${INVOICE_PLANE_FQDN}.conf
   sed -i 's,{{INVOICE_PLANE_FQDN}},'"${INVOICE_PLANE_FQDN}"',' /etc/nginx/sites-enabled/${INVOICE_PLANE_FQDN}.conf
   sed -i 's,{{INVOICE_PLANE_INSTALL_DIR}},'"${INVOICE_PLANE_INSTALL_DIR}"',' /etc/nginx/sites-enabled/${INVOICE_PLANE_FQDN}.conf
@@ -51,7 +51,7 @@ echo -n "Waiting for database server to accept connections"
 while ! mysqladmin -h ${DB_HOST} -P ${DB_PORT} -u ${DB_USER} ${DB_PASS:+-p$DB_PASS} status >/dev/null 2>&1
 do
   timeout=$(expr $timeout - 1)
-  if [ $timeout -eq 0 ]; then
+  if [[ $timeout -eq 0 ]]; then
     echo
     echo "Could not connect to database server. Aborting..."
     exit 1
@@ -62,7 +62,7 @@ done
 echo
 
 # create uploads directory
-if [ ! -d ${INVOICE_PLANE_DATA_DIR}/uploads ]; then
+if [[ ! -d ${INVOICE_PLANE_DATA_DIR}/uploads ]]; then
   cp -a ${INVOICE_PLANE_INSTALL_DIR}/uploads ${INVOICE_PLANE_DATA_DIR}/uploads
 fi
 rm -rf ${INVOICE_PLANE_INSTALL_DIR}/uploads
@@ -84,7 +84,8 @@ sudo -HEu ${INVOICE_PLANE_USER} sed -i 's,{{INVOICE_PLANE_TIMEZONE}},'"${INVOICE
 
 # create VERSION file, not used at the moment but might be required in the future
 CURRENT_VERSION=
-[ -f ${INVOICE_PLANE_DATA_DIR}/VERSION ] && CURRENT_VERSION=$(cat ${INVOICE_PLANE_DATA_DIR}/VERSION)
-[ "${INVOICE_PLANE_VERSION}" != "${CURRENT_VERSION}" ] && echo -n "${INVOICE_PLANE_VERSION}" > ${INVOICE_PLANE_DATA_DIR}/VERSION
+[[ -f ${INVOICE_PLANE_DATA_DIR}/VERSION ]] && CURRENT_VERSION=$(cat ${INVOICE_PLANE_DATA_DIR}/VERSION)
+[[ ${INVOICE_PLANE_VERSION} != ${CURRENT_VERSION} ]] && echo -n "${INVOICE_PLANE_VERSION}" > ${INVOICE_PLANE_DATA_DIR}/VERSION
 
+echo "Starting $@..."
 exec $@
