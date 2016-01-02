f18ed47334bdc9f03ed4ba06533acfc1f85b6a92
Sameer Naik <sameer@damagehead.com>
2016-01-02 12:51:54 +0530

    rename and reorg functions

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,16 +11,13 @@ elif [[ ${1} == php5-fpm || ${1} == $(which php5-fpm) ]]; then
   set --
 fi
 
-create_data_dir
-create_vhost_configuration
-autodetect_database_connection_parameters
-apply_database_settings
-configure_timezone
-update_volume_version
+initialize_datadir
+nginx_configure_virtualhost
+invoiceplane_configure_database
+invoiceplane_configure_timezone
 
 # default behaviour is to launch php5-fpm
 if [[ -z ${1} ]]; then
-  test_database_connection_settings
   exec start-stop-daemon --start --chuid root:root --exec $(which php5-fpm) -- ${EXTRA_ARGS}
 else
   exec "$@"
